8317793144f6c54777e1741319e3e57ea21c3964
Kotaro Kamashima <kotaro0726@gmail.com>
2024-05-11 10:57:50 +0900

    Update: Set up basic widgets

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_text_form_field_playground/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
